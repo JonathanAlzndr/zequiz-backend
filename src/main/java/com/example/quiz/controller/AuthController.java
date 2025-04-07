eb31fefedd19347adf3b7d528a7515c1d472272e
Jonathan K <jkamagi41@gmail.com>
2025-04-07 20:35:21 +0800

    adding login functionality

--- src/main/java/com/example/quiz/controller/AuthController.java
+++ src/main/java/com/example/quiz/controller/AuthController.java
@@ -1,17 +1,25 @@
 package com.example.quiz.controller;
 
 import com.example.quiz.entity.User;
+import com.example.quiz.model.LoginRequestBody;
+import com.example.quiz.model.LoginResponseBody;
 import com.example.quiz.model.RegisterRequest;
 import com.example.quiz.repository.UserRepository;
 import com.example.quiz.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("zequiz/auth")
@@ -28,7 +36,6 @@ public class AuthController {
 
     @Autowired
     JwtUtil jwtUtils;
-
 
     @PostMapping(
             path = "/register"
@@ -53,4 +60,45 @@ public class AuthController {
 
         return ResponseEntity.ok("User registered successfully");
     }
+
+    @PostMapping(
+            path = "/login"
+    )
+    public ResponseEntity<?> login(@RequestBody LoginRequestBody request) {
+
+        // Cek jika username telah ada di database
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow(
+                () -> new UsernameNotFoundException("Username not found : " + request.getUsername())
+        );
+
+        // cek jika user null atau tidak
+        if(user == null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Username not found");
+        }
+
+        // Cek jika password sesuai
+        if(!encoder.matches(request.getPassword(), user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid password");
+        }
+
+        // Jika password sesuai
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+
+        // Membuat token
+        String token = jwtUtils.generateToken(user.getUsername());
+        LoginResponseBody loginResponseBody = LoginResponseBody.builder()
+                .message("Login Successfully")
+                .token(token)
+                .build();
+
+        return ResponseEntity.ok(loginResponseBody);
+    }
+
 }
---
+++ src/main/java/com/example/quiz/model/LoginResponseBody.java
@@ -0,0 +1,15 @@
+package com.example.quiz.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginResponseBody {
+    private String message;
+    private String token;
+}
